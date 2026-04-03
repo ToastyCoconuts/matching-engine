9cc7a3895a2066eddb5df061aef5fd876b767a61
Samuel Heng <115957572+ToastyCoconuts@users.noreply.github.com>
2026-04-02 20:42:09 -0500

    Removed test file

--- main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <atomic>
-
-int main() {
-    std::atomic<int> x{0};
-    x.fetch_add(1, std::memory_order_relaxed);
-    std::cout <<"atomics work: " << x.load() << "\n";
-}
